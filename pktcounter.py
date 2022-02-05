6a4f16853d8ff5e9a88458d539175e7a8ee4f08d
Avor Sky <1737000@gmail.com>
2022-02-05 20:37:05 +0200

    Added docstrings

--- pktcounter.py
+++ pktcounter.py
@@ -5,10 +5,23 @@ from scapy.all import *
 
 @dataclass
 class PacketCounter:
+
     source_packets = {}
     destination_packets = {}
 
+
     def add_packet(self, ip: int, type: str) -> None:
+        """ Counts instances of certain IP and the number of its occurences. 
+            Stores data as source_packets, or destination_packets dict. 
+            Dict represents IPs as keys and number of occurences as values of type int.
+
+        Parameters:
+        ip(int): IP address to store.
+        type(str): Packet type. Supports values "source" or "destination".
+
+        Returns:
+        None
+        """
         if type == "source":
             if ip in self.source_packets:
                 self.source_packets[ip] += 1
@@ -20,7 +33,18 @@ class PacketCounter:
             else:
                 self.destination_packets[ip] = 1
     
+
     def print_packets(self, type: str, slice: int=10) -> None:
+        """ Prints desired type of counted packets in a table sorted 
+            from the hignest to the lowest value.
+        
+        Parameters:
+        type(str): Packet type. Supports values "source" or "destination".
+        slice(int): The slice of packets to print. Prints 10 rows by default.
+
+        Returns:
+        None
+        """
         if type == "source":
 
             print("\nSource packets: ")
@@ -44,12 +68,30 @@ class PacketCounter:
         else:
             print(f"Unknown type of packets specified: '{type}'. Please check for a typo.")
 
+
     @property
     def _top_source_ip(self) -> Any:
+        """ Property method to return the source IP with the hignest occurence value.
+
+        Parameters:
+        None 
+
+        Returns:
+        None
+        """
         sorted_packets = {k: v for k, v in sorted(self.source_packets.items(), key=lambda item: item[1], reverse=True)} 
         top_source = list(sorted_packets)[0]
         return top_source
 
+
     def trace_top_source(self):
+        """ Traces the route of the highest source IP to produce the structured representation.
+
+        Parameters:
+        None 
+
+        Returns:
+        None
+        """
         print(f'\nStructured representation of the top IP {self._top_source_ip}: \n')
         result, unans = traceroute(self._top_source_ip, maxttl=32)
